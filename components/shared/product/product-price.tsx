8324143dc85bd6cd2a790381a5c11ac356fef16e
ВьLachezar Uzunov <lachezar.z.uzunov@gmail.com>
2025-01-19 15:13:30 +0200

    add: product details page

--- components/shared/product/product-price.tsx
+++ components/shared/product/product-price.tsx
@@ -6,7 +6,8 @@ const ProductPrice = ({ value, className }: { value: number; className?: string}
 	return (
 		<p className={cn('text-2xl', className)}>
 			<span className='text-xs align-super'>$</span>
-			{ intValue }
+			{intValue}
+			<span className='text-xs align-super'>.{floatValue}</span>
 		</p>
 	)
 }
