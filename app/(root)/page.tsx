15faf46c709265f5899c3297814bce897fd10265
ВьLachezar Uzunov <lachezar.z.uzunov@gmail.com>
2025-01-19 14:21:24 +0200

    add: validators

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 import ProductList from "@/components/shared/product/product-list";
 
-const Homepage = () => {
+const Homepage = async () => {
+    const latestProducts = await getLatestProducts();
+
     return (
         <div className='my-10'>
             <ProductList
-                data={sampleData.products}
+                data={latestProducts}
                 title='Newest Arrivals'
                 limit={4}
             />
